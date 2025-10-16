ea0b166141f33834ef64d343269d618155d6cd18
Samuel Uribe Villegas <uribesamuel753@gmail.com>
2025-10-16 13:30:06 -0500

    Diseño inicio de sesión

---
+++ app/src/main/java/com/example/subtrack/ui/login/LoginActivity.kt
@@ -0,0 +1,30 @@
+package com.example.subtrack.ui.login
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.subtrack.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Por favor completa los campos", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
